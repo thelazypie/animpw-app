f7b0d4abbfc19fdc6a07026add330104be14daec
thelazypie <sven49376@gmail.com>
2021-04-29 00:49:44 +0300

    fuck

--- api/fetchTopAnime.js
+++ api/fetchTopAnime.js
@@ -1,7 +1,7 @@
 import fetchAnime from './fetchAnime'
 
 export default async function fetchTopAnime() {
-    const favoriteArr = await fetch(`http://youranime.me/api/anime/top?options=favorites`, { method: "GET" });
+    const favoriteArr = await fetch(`http://youranime.me/api/anime/top?options=favorites`, { method: "GET", headers: { 'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/83.0.4103.116 YaBrowser/20.7.3.100 Yowser/2.5 Yptp/1.23 Safari/537.36' } });
     const favoriteArrParsed = await favoriteArr.json();
 
     let favorites = [];
