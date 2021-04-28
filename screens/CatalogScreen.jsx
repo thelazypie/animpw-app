392a004584bf5928f67d4d31a0510208211e06c4
thelazypie <sven49376@gmail.com>
2021-04-29 02:23:22 +0300

    add a loading indicator on catalog and main screens

--- screens/CatalogScreen.jsx
+++ screens/CatalogScreen.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 
-import { ScrollView, TouchableOpacity, View } from 'react-native';
+import { ScrollView, TouchableOpacity, View, ActivityIndicator } from 'react-native';
 
 import AnimeHorizontal from '../components/Anime/AnimeHorizontal'
 
@@ -9,12 +9,16 @@ import fetchList from '../api/fetchList'
 export default function CatalogScreen({ navigation }) {
 
     let [animes, setAnimes] = React.useState([]);
+    const [loading, setloading] = React.useState(true);
 
     React.useEffect(() => {
         let isMounted = true;
 
         fetchList(0, 60).then((animeArr) => {
-            if (isMounted) setAnimes([...animeArr]);
+            if (isMounted) {
+                setAnimes([...animeArr]);
+                setloading(false);
+            }
         }).catch(() => {
             //TODO: something went wrong anime XD
         })
@@ -26,7 +30,7 @@ export default function CatalogScreen({ navigation }) {
 
 
     return (
-        <View>
+        loading ? <ActivityIndicator style={{ flex: 1, justifyContent: "center", alignContent: "center" }} size="large" color="#000000" /> : <View>
             <ScrollView>
                 {animes.map((anime, i) => {
                     return (
--- screens/MainScreen.jsx
+++ screens/MainScreen.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 
-import { View, ScrollView, TouchableOpacity, Text } from 'react-native';
+import { View, ScrollView, TouchableOpacity, Text, ActivityIndicator } from 'react-native';
 
 import fetchTopAnime from '../api/fetchTopAnime'
 import AnimeVertical from '../components/Anime/AnimeVertical'
@@ -8,11 +8,15 @@ import AnimeVertical from '../components/Anime/AnimeVertical'
 export default function CatalogScreen({ navigation }) {
 
     let [favorites, setfavorites] = React.useState([]);
+    const [loading, setloading] = React.useState(true);
 
     React.useEffect(() => {
         let isMounted = true;
         fetchTopAnime().then((fav) => {
-            if (isMounted) setfavorites([...fav]);
+            if (isMounted) {
+                setfavorites([...fav]);
+                setloading(false);
+            }
         });
         return () => {
             isMounted = false;
@@ -20,7 +24,7 @@ export default function CatalogScreen({ navigation }) {
     }, [])
 
     return (
-        <View style={{ flex: 1 }}>
+        loading ? <ActivityIndicator style={{ flex: 1, justifyContent: "center", alignContent: "center" }} size="large" color="#000000" /> : <View style={{ flex: 1 }}>
             <Text style={{ fontSize: 24, padding: 5 }}>Популярные аниме</Text>
             <ScrollView horizontal style={{ flex: 1 }}>
                 {favorites.map((favorite, i) => {
